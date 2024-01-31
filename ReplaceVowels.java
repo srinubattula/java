2f9196d9f25503ab894938437bd7b7ee328f1197
srinubattula <152388712+srinubattula@users.noreply.github.com>
2024-01-31 14:08:00 +0530

    Add files via upload

---
+++ ReplaceVowels.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class ReplaceVowels
+{
+	public static void main(String args[])
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter a string: ");
+		String str= sc.nextLine(); //reads string
+		System.out.print("The string after replacing vowels: ");
+		str = str.replaceAll("[aeiouAEIOU]", "@");  // Replace Vowels by $
+		System.out.println(str);
+	}
+}
